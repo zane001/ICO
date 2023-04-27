372a2cdefe783a604a88b41a1ee55168fe00a734
张猛 <zhangmeng717@zzbank.cn>
2023-04-27 18:09:08 +0800

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x85861b6c884Ee577437618a333343b54E8E76efB";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
